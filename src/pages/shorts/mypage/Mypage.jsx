f667e44896a9713d7d59d143f1f8075320b6440f
hwajun <hwajun15@naver.com>
2025-09-20 19:34:35 +0900

    [hwajun] feat : 프로필 편집 기능 구현완료

--- src/pages/shorts/mypage/Mypage.jsx
+++ src/pages/shorts/mypage/Mypage.jsx
@@ -133,20 +133,16 @@ const Mypage = () => {
           {/* 프로필 섹션 */}
           <div className="profile-section">
             <div className="profile-image">
-              {mypageData?.header?.avatarUrl ? (
-                <img
-                  src={`https://ohgoodpay2.s3.ap-northeast-2.amazonaws.com/${mypageData.header.avatarUrl}`}
-                  alt="프로필"
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    borderRadius: "50%",
-                    objectFit: "cover",
-                  }}
-                />
-              ) : (
-                <i className="fas fa-user" />
-              )}
+              <img
+                src="/src/features/shorts/img/profile.jpeg"
+                alt="프로필"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  borderRadius: "50%",
+                  objectFit: "cover",
+                }}
+              />
             </div>
             <div className="profile-info">
               <h2 className="username">
@@ -189,20 +185,16 @@ const Mypage = () => {
                   style={{ flexShrink: 0 }}
                 >
                   <div className="sub-profile">
-                    {item.avatarUrl ? (
-                      <img
-                        src={`https://ohgoodpay2.s3.ap-northeast-2.amazonaws.com/${item.avatarUrl}`}
-                        alt="구독자 프로필"
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          borderRadius: "50%",
-                          objectFit: "cover",
-                        }}
-                      />
-                    ) : (
-                      <i className="fas fa-user" />
-                    )}
+                    <img
+                      src="/src/features/shorts/img/profile2.png"
+                      alt="구독자 프로필"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                      }}
+                    />
                   </div>
                   <span className="sub-name">{item.displayName}</span>
                 </div>
