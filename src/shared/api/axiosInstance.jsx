9b3774a29047646109bf3e267a7f45c18e24772c
hwajun <hwajun15@naver.com>
2025-09-26 15:39:47 +0900

    [hwajun] feat : axioInstance 수정 및 비회원일때 기능 수정정

--- src/shared/api/axiosInstance.jsx
+++ src/shared/api/axiosInstance.jsx
@@ -23,9 +23,11 @@ axiosInstance.interceptors.response.use(
     ) {
       console.error("인증 오류: 토큰이 유효하지 않거나 만료되었습니다.");
       sessionStorage.removeItem("accessToken"); // 토큰 제거
-
-      // 현재 페이지가 로그인 페이지가 아닐 때만 리디렉션
-      if (window.location.pathname !== "/login") {
+      
+      // if (!isProfileEditRequest && window.location.pathname !== "/login") {
+      //   window.location.href = "/login"; // 로그인 페이지로 리디렉션
+      // }
+      if(window.location.pathname !== "/login") {
         window.location.href = "/login"; // 로그인 페이지로 리디렉션
       }
     }
@@ -36,15 +38,17 @@ axiosInstance.interceptors.response.use(
 axiosInstance.interceptors.request.use(
   async (config) => {
     // public API나 auth 관련 API는 토큰 없이 요청
+    console.log("config.url:", config.url);
     if (
-      (config.url.includes("/api/public") || config.url.includes("/auth")) &&
-      !config.url.includes("/api/public/shorts")
+      config.url.includes("/api/public") || config.url.includes("/auth") ||
+      config.url.includes("/api/public/shorts") || config.url.includes("/api/public/shorts/feeds?")
     ) {
       return config;
     }
 
     try {
       const token = await callToken();
+      console.log("config.url:", config.url);
 
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -52,13 +56,22 @@ axiosInstance.interceptors.request.use(
         if (config.url.includes("/api/public/shorts")) {
           return config;
         }
+        if(config.url.includes("/api/public/shorts/")) {
+          return config;
+        }
+        
+        
         // 토큰이 없으면 로그만 출력하고 요청을 계속 진행
         // 서버에서 401을 반환하면 응답 인터셉터에서 처리
         console.warn("토큰이 없습니다. 서버에서 인증을 확인합니다.");
+        // 프로필 수정 관련 요청은 자동 리다이렉트하지 않음
+        
+        
         if (window.location.pathname !== "/login") {
-          window.location.href = "/login"; // 로그인 페이지로 리디렉션
-        }
-        return;
+            window.location.href = "/login"; // 로그인 페이지로 리디렉션
+            return config;
+          }
+        
       }
     } catch (error) {
       console.error("토큰 처리 중 오류 발생:", error);
