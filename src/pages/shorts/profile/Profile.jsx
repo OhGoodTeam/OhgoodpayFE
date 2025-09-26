288cbd086465a96d5c44a5f12c710f31e9083de1
오가희 <ogh010@naver.com>
2025-09-26 17:35:34 +0900

    [gahee] feat : css 수정

--- src/pages/shorts/profile/Profile.jsx
+++ src/pages/shorts/profile/Profile.jsx
@@ -186,13 +186,13 @@ const Profile = () => {
                 case "SELF":
                   return (
                     <div style={{ display: "flex", gap: "10px" }}>
-                      <SubscribeButton
+                      {/* <SubscribeButton
                         value="동영상 관리"
                         onClick={() => {
                           navigate(`/shorts/profile/all?targetId=${targetId}`);
                         }}
                         option="flex"
-                      />
+                      /> */}
                       <SubscribeButton
                         value="프로필 편집"
                         onClick={() => {
